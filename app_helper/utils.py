62516a6449931f486bbcadee19d598a6809d2b10
Moritz Pietzschke <git@mopie.de>
2022-02-14 17:49:11 +0100

    move SECRET_KEY setting down;

--- app_helper/utils.py
+++ app_helper/utils.py
@@ -262,11 +262,11 @@ def _make_settings(args, application, settings, STATIC_ROOT, MEDIA_ROOT):  # NOQ
     if application not in default_settings["INSTALLED_APPS"]:
         default_settings["INSTALLED_APPS"].append(application)
 
-    if "SECRET_KEY" not in default_settings:
-        default_settings["SECRET_KEY"] = "test"
-
     if not migrate:
         default_settings["MIGRATION_MODULES"] = DisableMigrations()
+
+    if "SECRET_KEY" not in default_settings:
+        default_settings["SECRET_KEY"] = "test"
 
     if "MIDDLEWARE" not in default_settings:
         default_settings["MIDDLEWARE"] = default_settings["MIDDLEWARE_CLASSES"]
